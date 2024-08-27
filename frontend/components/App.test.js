acdbb4bff7ca0b9bc6396fc57d2bc7fe161d7797
Whitneyy <whitneyyx77@gmail.com>
2024-08-27 00:24:54 -0400

    Completed Sprint Challenge Advanced React

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,4 +1,21 @@
-// Write your tests here
-test('sanity', () => {
-  expect(true).toBe(false)
-})
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import AppFunctional from './AppFunctional';
+
+test('renders all headings and buttons', () => {
+  render(<AppFunctional />);
+  expect(screen.getByText(/coordinates/i)).toBeInTheDocument();
+  expect(screen.getByText(/you moved/i)).toBeInTheDocument();
+  expect(screen.getByText(/left/i)).toBeInTheDocument();
+  expect(screen.getByText(/up/i)).toBeInTheDocument();
+  expect(screen.getByText(/right/i)).toBeInTheDocument();
+  expect(screen.getByText(/down/i)).toBeInTheDocument();
+  expect(screen.getByText(/reset/i)).toBeInTheDocument();
+});
+
+test('typing in the input changes its value', () => {
+  render(<AppFunctional />);
+  const emailInput = screen.getByPlaceholderText(/ type email/i);
+  fireEvent.change(emailInput, { target: { value: 'test@test.com' } });
+  expect(emailInput.value).toBe('test@test.com');
+});
