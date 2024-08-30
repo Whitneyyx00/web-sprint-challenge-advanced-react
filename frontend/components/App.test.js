89f153b4387bfedd0073297d0d8c5fcb5977935d
Whitneyy <whitneyyx77@gmail.com>
2024-08-30 17:17:39 -0400

    Edited errors to successfully complete sprint

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import AppFunctional from './AppFunctional';
+import AppClass from'./AppClass';
+import '@testing-library/jest-dom';
 
-test('renders all headings and buttons', () => {
+// Tests for AppFuntional
+test('renders all headings and buttons for AppFunctional', () => {
   render(<AppFunctional />);
   expect(screen.getByText(/coordinates/i)).toBeInTheDocument();
   expect(screen.getByText(/you moved/i)).toBeInTheDocument();
@@ -13,9 +16,28 @@ test('renders all headings and buttons', () => {
   expect(screen.getByText(/reset/i)).toBeInTheDocument();
 });
 
-test('typing in the input changes its value', () => {
+test('typing in the input changes its value for AppFunctional', () => {
   render(<AppFunctional />);
-  const emailInput = screen.getByPlaceholderText(/ type email/i);
+  const emailInput = screen.getByPlaceholderText(/type email/i);
+  fireEvent.change(emailInput, { target: { value: 'test@test.com' } });
+  expect(emailInput.value).toBe('test@test.com');
+});
+
+// Tests for AppClass
+test('renders all headings and buttons for AppClass', () => {
+  render(<AppClass />);
+  expect(screen.getByText(/coordinates/i)).toBeInTheDocument();
+  expect(screen.getByText(/you moved/i)).toBeInTheDocument();
+  expect(screen.getByText(/left/i)).toBeInTheDocument();
+  expect(screen.getByText(/up/i)).toBeInTheDocument();
+  expect(screen.getByText(/right/i)).toBeInTheDocument();
+  expect(screen.getByText(/down/i)).toBeInTheDocument();
+  expect(screen.getByText(/reset/i)).toBeInTheDocument();
+});
+
+test('typing in the input changes its value for AppClass', () => {
+  render(<AppClass />);
+  const emailInput = screen.getAllByPlaceholderText(/type email/i)[0];
   fireEvent.change(emailInput, { target: { value: 'test@test.com' } });
   expect(emailInput.value).toBe('test@test.com');
 });
